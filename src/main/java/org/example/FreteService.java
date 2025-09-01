f0781b15be542baba22375108a5ca05478935f0f
André Luiz da Silva <andreluizdasilva@MacBook-Air-de-Andre.local>
2025-09-01 20:10:04 -0300

    v2

--- src/main/java/org/example/FreteService.java
+++ src/main/java/org/example/FreteService.java
@@ -11,11 +11,41 @@ public class FreteService {
     private static final double MAXIMO = 300.0;
 
     public double calcularFrete(Pedido pedido){
-        // Passo 1: Validar dados de entrada
+        // Valor Total do Frete
+        double total = 0.0;
+
+        // Regra 1: Validar dados de entrada
         validar(pedido);
 
-        //Implementar...
-        return 0;
+        // Regra 2: calcular o valor com base na distancia percorrida
+        total += pedido.getDistanciaKm() * BASE_POR_KM;
+
+        // Regra 3: calcula o valor com base no excesso de peso
+        if (pedido.getPesoKg() > EXCESSO_PESO_LIMITE_KG){
+            double excesso = pedido.getPesoKg() - EXCESSO_PESO_LIMITE_KG;
+            total += excesso * EXCESSO_PESO_TAXA_POR_KG;
+        }
+
+        // Regra 4: Verificar se o produto é fragil.
+        if (pedido.isFragil()){
+            total += TAXA_FRAGIL;
+        }
+
+        // Regra 5: Verifico se o tipo de frete é expresso
+        if (pedido.isExpresso()){
+            total *= MULTIPLICADOR_EXPRESSO;
+        }
+
+        // Regra 6: piso/teto
+        total = Math.max(MINIMO, Math.min(MAXIMO, total));
+
+        // Regra 7: Frete Gratis : valor > 200.0 e a
+        // distancia for menor ou igual a 20 km e não foi frete expresso
+        if (!pedido.isExpresso() && pedido.getValorItens() >= 200.0 && pedido.getDistanciaKm() <= 20.0){
+            return 0.0;
+        }
+
+        return total;
     }
 
     private  void validar (Pedido pedido){
@@ -35,11 +65,19 @@ public class FreteService {
         Pedido gratis = new Pedido(250.0, 15.0, 3.0,
                 false, false);
 
+        Pedido test = new Pedido(50.0,10.0, 3.0, false, false);
+
+        Pedido gr2 = new Pedido(220.0, 20.0, 2.0,
+                false, false);
+
         FreteService fs = new FreteService();
 
         System.out.println("Frete Padrão: " + fs.calcularFrete(padrao));
         System.out.println("Frete Frágil: " + fs.calcularFrete(fragil));
         System.out.println("Frete Expresso: " + fs.calcularFrete(expresso));
         System.out.println("Frete Grátis: " + fs.calcularFrete(gratis));
+
+        System.out.println("Teste: " + fs.calcularFrete(test));
+        System.out.println("gr2: " + fs.calcularFrete(gr2));
     }
 }
--- src/test/java/FreteServiceTest.java
+++ src/test/java/FreteServiceTest.java
@@ -1,9 +1,12 @@
+import jdk.jshell.PersistentSnippet;
 import org.example.FreteService;
 import org.example.Pedido;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class FreteServiceTest {
     private final FreteService fs = new FreteService();
+    private static final double DELTA = 0.0001;
 
     @Test (expected = IllegalArgumentException.class)
     public void validarDeveRejeitarDistanciaNegativa(){
@@ -16,5 +19,46 @@ public class FreteServiceTest {
         fs.calcularFrete(new
                 Pedido(50.0, 10.0, -5, true,true));
     }
+
+    @Test
+    public void validarBasePorDistanciaPercorrida(){
+        Pedido p = new Pedido(50.0,10.0, 3.0, false, false);
+        //10km -> 1.2 -> minimo é 10, 12 reais
+        Assert.assertEquals(12.0, fs.calcularFrete(p), DELTA);
+    }
+
+    @Test
+    public void validarExcessoPesoAcimaLimite(){
+        Pedido pNoLimite = new Pedido(50.0, 10.0,
+                5.0, false, false);
+        Pedido pAcima = new Pedido(50.0, 10.0,
+                5.5, false, false);
+        // No limite, 10*1.2 = 12
+        Assert.assertEquals(12.0, fs.calcularFrete(pNoLimite), DELTA);
+        // Peso foi mais que 5kg -> 10*1.2 = 12 + 0.5*2 -> 12 + 1 => 13
+        Assert.assertEquals(13.0, fs.calcularFrete(pAcima), DELTA);
+    }
+
+    @Test
+    public void validarTaxaFragil(){
+        Pedido p = new Pedido(80.0, 10.0, 6.0, true, false);
+        // 10 * 1.2 = 12 + excesso (1kg * 2) = 2 = 12 + 2 => 14 => + fragil 15 => 29.00 reais
+        Assert.assertEquals(29.00, fs.calcularFrete(p), DELTA);
+    }
+
+    @Test
+    public void validarFreteExpresso(){
+        Pedido p = new Pedido(90.0, 5.0, 2.0, false, true);
+        // 1.2 * 5 => 6 => * 1,5 => 9.00 (> 10 minimo) => 10 reais
+        Assert.assertEquals(10.0, fs.calcularFrete(p), DELTA);
+    }
+
+    @Test
+    public void freteGratis(){
+        Pedido gratis = new Pedido(220.0, 20.0, 2.0,
+                false, false);
+        Assert.assertEquals(0.0, fs.calcularFrete(gratis), DELTA);
+    }
+
 
 }
